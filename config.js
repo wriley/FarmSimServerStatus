cfc4a789ef13b17dc648227042c2e16eec62824c
William Riley <wrileystl@gmail.com>
2018-03-17 11:49:17 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // Change IP address, port, and code to match your server
+// The code is on the Settings -> Miscellaneous page of the admin portal
 url = "http://192.168.1.1:8080/feed/dedicated-server-stats.xml?code=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
