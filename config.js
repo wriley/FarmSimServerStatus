22531874b90e91474b3a2ef27d9c51f1e2d05b43
William Riley <wrileystl@gmail.com>
2018-03-19 08:41:21 -0500

    added player farms

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // Change IP address, port, and code to match your server
 // The code is on the Settings -> Miscellaneous page of the admin portal
 url = "http://192.168.1.1:8080/feed/dedicated-server-stats.xml?code=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+url2 = "http://192.168.1.1:8080/feed/dedicated-server-savegame.html?code=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx&file=careerSavegame"
